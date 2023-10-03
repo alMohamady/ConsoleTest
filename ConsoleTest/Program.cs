63c6e2a3b98e188a20ca50c6fee24e6a2eb92dff
al Mohamady <al.Mohamady47@gmail.com>
2023-10-03 17:10:40 +0300

    05 Anonymous Types in C#

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,18 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using ConsoleTest.Extentions;
-using SeedData;
-
-Console.WriteLine("Hello, World!");
-
-//var ahmedList = Filter.getAhmeds(GetData.GetCustomers());
-var ahmedList = GetData.GetCustomers().getByName("mohmed");
-
-
-foreach (var ahmed in ahmedList)
-{
-    Console.WriteLine($" name {ahmed.name} : age {ahmed.age} ");
-}
 
 
 
 
+var c1 = new { id = 101, name = "ahmed mohmed", age = 30, isActive = true};
+var c2 = new { id = 102, name = "ahmed ", age = 31, isActive = true };
+
+var c3 = new { id = 102,  age = 31,name = "ahmed " };
+
+var c4 = new { id = 101, name = "ahmed mohmed", age = 30, isActive = true };
+
+Console.WriteLine(c1.GetType().Name);
+Console.WriteLine(c2.GetType().Name);
+Console.WriteLine(c3.GetType().Name);
+
+Console.WriteLine(c1.Equals(c4));
+
+var c5 = new { custId = 101, custName = "ahmed mohmed", custAge = 30, isActive = true };
+
+Console.WriteLine(c5.GetType().Name);
+
+Console.WriteLine(c1.Equals(c5));
