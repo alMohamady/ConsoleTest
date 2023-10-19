165a8014f180423bdd44af0a1c92ad0ae92e5201
al Mohamady <al.Mohamady47@gmail.com>
2023-10-19 12:39:29 +0300

    09 Select Operator in LINQ

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,24 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using SeedData;
+using SeedData.Models;
 using System.Linq;
 
-List<int> myList = new List<int> { 1, 2, 3, 4, 5, 6 , 7, 8 , 9 , 10 };
-List<string> stringList = new List<string>
-             { "ahmed mohamady", "mohamed ahmed", "mo salah", "sayd ahmed" };
+//List<int> myList = new List<int> { 1, 2, 3, 4, 5, 6 , 7, 8 , 9 , 10 };
+//List<string> stringList = new List<string>
+//             { "ahmed mohamady", "mohamed ahmed", "mo salah", "sayd ahmed" };
 
 
-var names = stringList.Where(x => x.Contains("ahmed"));
+var customers = GetData.GetCustomers().Where(x => x.age > 30).Select(
+       a => new CustomerDto
+       {
+           customerName = a.name,
+           customerPhone = a.telephone,
+       }
+    );
 
-//foreach (var name in names)
-//{
-//    Console.WriteLine(name);  
-//}
+var customers2 = from cust in GetData.GetCustomers()
+               where cust.age > 30
+               select new 
+               {
+                   customerName = cust.name,
+                   customerPhone = cust.telephone,
+               };
 
-stringList.Remove("mohamed ahmed");
-stringList.AddRange(new string[] { "saeed salah", "ahmed morad", "ahmed ahmed" });
 
-foreach (var name in names)
+foreach (var customer in customers)
 {
-    Console.WriteLine(name);
+    Console.WriteLine(customer.customerName);
 }
---
+++ SeedData/Models/CustomerDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeedData.Models
+{
+    public  class CustomerDto
+    {
+       public string  customerName { get; set; }
+        public long customerPhone { get; set; }
+    }
+}
