9960d1083cf40e49679d9e28a8fdaeba75877334
al Mohamady <al.Mohamady47@gmail.com>
2023-11-14 19:34:21 +0200

    18 Set & Casting Operators in Linq

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -5,28 +5,21 @@ using SeedData;
 using SeedData.Models;
 using System.Linq;
 
-var r = Enumerable.Range(1, 1000);
+var result = GetData.GetCustomers().Where(x => x.categoryId == 1);
+//.ToList();
+//.ToArray();
+//.ToDictionary(x => x.categoryId);
 
-var e = Enumerable.Empty<Customer>();
 
-var rep = Enumerable.Repeat(100, 4);
+var l1 = Enumerable.Range(0, 10);
+var l2 = Enumerable.Range(5, 15);
 
-var cust = GetData.GetCustomers().FirstOrDefault();
-var rep2 = Enumerable.Repeat(cust, 4);
+//var ls = l1.Union(l2);
+//var ls = l1.Concat(l2);
+//ls = ls.Distinct();
 
-//====================================
+//var ls = l1.Except(l2);
+var ls = l1.Intersect(l2);
 
-List<string> names = new List<string>() 
-{ "Ahmed Moahamdy", "Mohamed Salah", "alaa khaled"};
-
-var sm = names.SelectMany(element => element.Split(' '));
-
-foreach (var name in sm)
-    Console.WriteLine(name);
-
-var smq = from n in names
-          from ns in n.Split(" ")
-          select ns;
-
-foreach (var name in smq)
-    Console.WriteLine(name);
+foreach (var x in ls)
+    Console.WriteLine(x);
