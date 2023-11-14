95c3a9294f7ebb3cb74b39e214adce7793531e2b
al Mohamady <al.Mohamady47@gmail.com>
2023-11-14 21:28:35 +0200

    19 Quantifier operations in LINQ

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -3,23 +3,30 @@
 
 using SeedData;
 using SeedData.Models;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
-var result = GetData.GetCustomers().Where(x => x.categoryId == 1);
-//.ToList();
-//.ToArray();
-//.ToDictionary(x => x.categoryId);
+//var isAllAge = GetData.GetCustomers().All(e => e.name.Contains("ahmed"));
+//Console.WriteLine(isAllAge.ToString());
 
 
-var l1 = Enumerable.Range(0, 10);
-var l2 = Enumerable.Range(5, 15);
+//var isAnyAge = GetData.GetCustomers().Any(e => e.name.Contains("ahmed"));
+//Console.WriteLine(isAnyAge.ToString());
 
-//var ls = l1.Union(l2);
-//var ls = l1.Concat(l2);
-//ls = ls.Distinct();
+var cust = new Customer { id = 101, name = "ahmed mohmed", age = 30, isActive = true, joinDate = new DateTime(2022, 10, 15), categoryId = 1, spendAverage = 1500.9m, telephone = 123456789 };
 
-//var ls = l1.Except(l2);
-var ls = l1.Intersect(l2);
+var isContAge = GetData.GetCustomers().Contains(cust, new CustCompar());
+Console.WriteLine(isContAge.ToString());
 
-foreach (var x in ls)
-    Console.WriteLine(x);
+public class CustCompar : IEqualityComparer<Customer>
+{
+    public bool Equals(Customer? x, Customer? y)
+    {
+        return x.id == y.id;
+    }
+
+    public int GetHashCode([DisallowNull] Customer obj)
+    {
+        return obj.id.GetHashCode();
+    }
+}
